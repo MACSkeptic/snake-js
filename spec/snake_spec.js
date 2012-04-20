92a6449111178d5df3a69afd5406408e43a6dc7b
mozair <macskeptic@gmail.com>
2012-04-20 15:03:10 -0300

    direction know is aware of its siblings

--- spec/snake_spec.js
+++ spec/snake_spec.js
@@ -7,23 +7,47 @@ describe('all', function () {
 
 describe('direction', function () {
   describe('north', function () {
-    it('should provide the right delta', function () {
+    it('should know the next position', function () {
       expect(Snake.direction.north.next(Snake.position(1, 1))).toEqual(Snake.position(1, 2));
+    });
+    it('should know the direction that is to the left of it', function () {
+      expect(Snake.direction.north.left()).toEqual(Snake.direction.west);
+    });
+    it('should know the direction that is to the right of it', function () {
+      expect(Snake.direction.north.right()).toEqual(Snake.direction.east);
     });
   });
   describe('south', function () {
-    it('should provide the right delta', function () {
+    it('should know the next position', function () {
       expect(Snake.direction.south.next(Snake.position(1, 1))).toEqual(Snake.position(1, 0));
+    });
+    it('should know the direction that is to the left of it', function () {
+      expect(Snake.direction.south.left()).toEqual(Snake.direction.east);
+    });
+    it('should know the direction that is to the right of it', function () {
+      expect(Snake.direction.south.right()).toEqual(Snake.direction.west);
     });
   });
   describe('west', function () {
-    it('should provide the right delta', function () {
+    it('should know the next position', function () {
       expect(Snake.direction.west.next(Snake.position(1, 1))).toEqual(Snake.position(0, 1));
+    });
+    it('should know the direction that is to the left of it', function () {
+      expect(Snake.direction.west.left()).toEqual(Snake.direction.south);
+    });
+    it('should know the direction that is to the right of it', function () {
+      expect(Snake.direction.west.right()).toEqual(Snake.direction.north);
     });
   });
   describe('east', function () {
-    it('should provide the right delta', function () {
+    it('should know the next position', function () {
       expect(Snake.direction.east.next(Snake.position(1, 1))).toEqual(Snake.position(2, 1));
+    });
+    it('should know the direction that is to the left of it', function () {
+      expect(Snake.direction.east.left()).toEqual(Snake.direction.north);
+    });
+    it('should know the direction that is to the right of it', function () {
+      expect(Snake.direction.east.right()).toEqual(Snake.direction.south);
     });
   });
 });
--- src/snake.js
+++ src/snake.js
@@ -3,10 +3,34 @@ var Snake = Snake || {};
 Snake.direction = (function () {
   var api = {};
 
-  api.east  = (function () { return { next: function(position) { return position.add(1, 0); } }; }());
-  api.west  = (function () { return { next: function(position) { return position.add(-1, 0); } }; }());
-  api.north = (function () { return { next: function(position) { return position.add(0, 1); } }; }());
-  api.south = (function () { return { next: function(position) { return position.add(0, -1); } }; }());
+  api.east = (function () { 
+    return { 
+      next: function (position) { return position.add(1, 0); },
+      left: function () { return Snake.direction.north },
+      right: function () { return Snake.direction.south }
+    }; 
+  }());
+  api.west = (function () { 
+    return { 
+      next: function (position) { return position.add(-1, 0); },
+      left: function () { return Snake.direction.south },
+      right: function () { return Snake.direction.north }
+    }; 
+  }());
+  api.south = (function () { 
+    return { 
+      next: function (position) { return position.add(0, -1); },
+      left: function () { return Snake.direction.east },
+      right: function () { return Snake.direction.west }
+    }; 
+  }());
+  api.north = (function () { 
+    return { 
+      next: function (position) { return position.add(0, 1); },
+      left: function () { return Snake.direction.west },
+      right: function () { return Snake.direction.east }
+    }; 
+  }());
 
   return api;
 }());
