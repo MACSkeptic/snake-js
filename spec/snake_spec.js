6b4ac27fb506c0b916f77bc0f2dbc50b353c434c
mozair <macskeptic@gmail.com>
2012-04-20 12:38:04 -0300

    added equality for positions

--- spec/snake_spec.js
+++ spec/snake_spec.js
@@ -4,3 +4,18 @@ describe('all', function () {
    expect([1, 2, 3].all(function (e) { return e < 3; } )).toBeFalsy();
  });  
 });
+
+describe('position', function () {
+  describe('equality', function () {
+    it('should be true if the coordinates are the same', function () {
+      var a = Snake.position(1, 2);
+      var b = Snake.position(1, 2);
+      expect(a).toEqual(b);
+    });
+    it('should be false if the coordinates are not the same', function () {
+      var a = Snake.position(1, 2);
+      var b = Snake.position(1, 1);
+      expect(a).not.toEqual(b);
+    });
+  });
+});
--- src/snake.js
+++ src/snake.js
@@ -6,4 +6,18 @@ Snake.snake = function () {
   
 
   return api;
-}
+};
+
+Snake.position = function (x, y) {
+  var api = {};
+
+  api.x = function () { return x; };
+  api.y = function () { return y; };
+
+  api.equals = function (otherPosition) {
+    return x == otherPosition.x() &&
+      y == otherPosition.y();
+  };
+
+  return api;
+};
