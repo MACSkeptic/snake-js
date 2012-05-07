b0587ea16003da3ad313915672a8645f0a829a75
mozair <macskeptic@gmail.com>
2012-05-07 15:12:20 -0300

    adding functionality of position

--- src/position.js
+++ src/position.js
@@ -1,7 +1,17 @@
 var Snake = Snake || {};
 
-Snake.position = function () {
+Snake.position = function (x, y) {
   var api = {};
+
+  api.x = function () { return x; };
+  api.y = function () { return y; };
+
+  api.equals = function (otherPosition) { return x == otherPosition.x() && y == otherPosition.y(); };
+  api.toString = function () { return '# Position: (' + x + ', ' + y + ') #'; };
+
+  api.between = function (lower, upper) { return lower.x() < x && lower.y() < y && upper.x() > x && upper.y() > y; };
+
+  api.add = function (dx, dy) { return Snake.position(x + dx, y + dy); };
 
   return api;
 };
