0c74fc31f7828de51ed592866044a651bdc1726a
mozair <macskeptic@gmail.com>
2012-05-07 15:28:00 -0300

    added implementation for board

--- src/board.js
+++ src/board.js
@@ -1,7 +1,14 @@
 var Snake = Snake || {};
 
-Snake.board = function () {
+Snake.board = function (size) {
   var api = {};
+
+  function random() {
+    return Math.floor(Math.random() * size); 
+  }
+
+  api.contains = function (position) { return position.between(Snake.position(-1, -1), Snake.position(size, size)); };
+  api.randomPosition = function () { return Snake.position(random(), random()); };
 
   return api;
 };
