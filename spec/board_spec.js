4fddef1aba60273d2026f574e71d4b60921f019b
mozair <macskeptic@gmail.com>
2012-05-07 15:27:23 -0300

    added tests for board

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -1,2 +1,17 @@
 describe('board', function () {
+  it('should verifiy if a position is inside its boundaries', function () {
+    var board = Snake.board(10);
+    expect(board.contains(Snake.position(0, 0))).toBeTruthy();
+    expect(board.contains(Snake.position(9, 9))).toBeTruthy();
+    expect(board.contains(Snake.position(10, 9))).toBeFalsy();
+    expect(board.contains(Snake.position(9, 10))).toBeFalsy();
+    expect(board.contains(Snake.position(-1, 0))).toBeFalsy();
+    expect(board.contains(Snake.position(0, -1))).toBeFalsy();
+  });
+
+  it('should pick a random spot', function () {
+    var board = Snake.board(20);
+    var position = board.randomPosition();
+    expect(board.contains(position)).toBeTruthy();
+  });
 });
